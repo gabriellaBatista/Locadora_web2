e2396d06d3042ac6ca9d935921ed2f7d3d066469
gabriella Batista <gabriella10Batista@gmail.com>
2021-03-05 07:18:47 -0300

    Fazendo relacionamento entre tabelas

--- Locadora1/app/Locacao.php
+++ Locadora1/app/Locacao.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Locacao extends Model
 {
    use HasFactory;
+   
    public function locacaos(){
-   	return $this->belongsToMany(Locacao::class)
+   	return $this->belongsToMany(Filme::class,'filme_locacaos')->withPivot('quantidade');
    }
 }
