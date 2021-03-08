d9b8bd533ac9563cead8defbed7f0861ec24895b
gabriella Batista <gabriella10Batista@gmail.com>
2021-03-08 13:15:27 -0300

    Adicionando filmes as locações

--- Locadora1/database/factories/FilmeLocacaoFactory.php
+++ Locadora1/database/factories/FilmeLocacaoFactory.php
@@ -1,12 +1,33 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
+use App\Models\FilmeLocacao;
+use illuminate\Database\Eloquent\Factories\Factory;
+
+class FilmeLocacaoFactory extends Factory
+{
+    /**
+     *The name of the factory's corresponding model.
+     *
+     *@var string 
+     */
+    protected $modal = FilmeLocacao::class;
+
+     /**
+      *Define the modal's defalt state.
+      *
+      *@return array
+      */
+    public function definition()
+    {
 use App\FilmeLocacao;
 use Faker\Generator as Faker;
 
 $factory->define(FilmeLocacao::class, function (Faker $faker) {
     return [
-        //
-    ];
-});
+        'quantidade' => $this->faker->randomDigit
+     ];
+   }
+ });
+}
