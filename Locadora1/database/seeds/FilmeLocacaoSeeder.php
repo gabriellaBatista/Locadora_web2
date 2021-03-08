ed25bf90724241f9a9e260eb08a98afc09622167
gabriella Batista <gabriella10Batista@gmail.com>
2021-03-08 17:06:34 -0300

    Adicionando tabela intermediária entre filme e locação

--- Locadora1/database/seeds/FilmeLocacaoSeeder.php
+++ Locadora1/database/seeds/FilmeLocacaoSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace Database\Seeders;
+
+use App\Models\Filme;
+use App\Models\Locacao;
+use App\Models\FilmeLocacao;
 use Illuminate\Database\Seeder;
 
 class FilmeLocacaoSeeder extends Seeder
@@ -11,6 +16,16 @@ class FilmeLocacaoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (Locacao::all() as $locacao) {
+            $filmes = Filme::all()->where('user_id', $locacao->user_Id)->randon(3);
+            foreach($filmes as $filme) {
+           	FilmeLocacao::factory(1)->create([
+          	    'filme_id' => $filme->id,
+          	    'locacao_id' => $locacao->id
+
+          	])
+
+          }
+       }
     }
 }
