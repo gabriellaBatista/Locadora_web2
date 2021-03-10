e0dc2954ced873784990109d6813c4b16969d2f0
gabriella Batista <gabriella10Batista@gmail.com>
2021-03-10 18:51:55 -0300

    Adicionando atributos do usuário e registrar

--- Locadora1/database/factories/UserFactory.php
+++ Locadora1/database/factories/UserFactory.php
@@ -3,9 +3,15 @@ protected $model = User::class;
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
+ use App\Models\User;
+ use Illuminate\Database\Eloquent\Factories\Factory;
  use Faker\Generator as Faker;
  use Illuminate\Support\Str;
+
+ class UserFactory extends Factory
+ {
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +28,17 @@ public function definition()
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' =>$this-> $faker->name,
-        'email' =>$this->$faker->unique()->safeEmail,
+        'name' => $this-> $faker->name,
+        'email' => $this->$faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'cep'=> $this->faker->nemerify('#####-###'),
+        'numero'=> $this->faker->randomNumber(3, false),
+        //'cpf'=> $this->faker->unique()->numerify('###.###.###-##'),
+        'cpf'=> $this->faker->unique()->cpf(),
+
     ];
 });
  }
+   }
